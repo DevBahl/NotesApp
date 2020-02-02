839b92bdc924c3a7cd054f5c382fb5b215e70152
Dev Bahl <devbahl12@gmail.com>
2020-02-02 23:06:05 +0530

    Initial Commit

---
+++ NotesApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NotesApp
+//
+//  Created by Dev Bahl on 02/02/20.
+//  Copyright © 2020 Dev Bahl. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}

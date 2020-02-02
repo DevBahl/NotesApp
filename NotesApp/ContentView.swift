f19b38fa0945e583b0d4d716bda6836e28d99947
Dev Bahl <devbahl12@gmail.com>
2020-02-03 01:06:53 +0530

    initial files

--- NotesApp/ContentView.swift
+++ NotesApp/ContentView.swift
@@ -9,10 +9,37 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @Environment(\.managedObjectContext) var managedObjectContext
+    
+    @FetchRequest(fetchRequest: ToDoItem.getAllToDoItems()) var toDoItems:FetchedResults<ToDoItem>
+    
+    @State private var newToDoItem = ""
+    
     var body: some View {
-        Text("Hello, World!")
+        NavigationView{
+            List{
+                Section(header: Text("What's Next?")){
+                    HStack{
+                        TextField("New Item", text: $newToDoItem)
+                        Button(action:{
+                            
+                        }){
+                            Image(systemName: "plus.circle.fill")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width:30,height:30)
+                                .foregroundColor(.green)
+                        }
+                    }
+                }.font(.headline)
+            }
+            .navigationBarTitle(Text("My Notes"))
+            .navigationBarItems(trailing: EditButton())
+        }
     }
 }
+    
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
